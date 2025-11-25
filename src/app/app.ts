7f976ab6daaeb4fe7438e9188f4e6b250fc73b06
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-11-25 17:45:38 +0000

    ajustados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService, CukThemeConfig } from 'corporate-ui-kit';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,20 @@ import { RouterOutlet } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = signal('corporate-ui-kit');
+  title = signal('corporate-ui-kit-showcase');
+  private themeService = inject(ThemeService);
+
+  constructor() {
+    this.initializeTheme();
+  }
+
+  private initializeTheme(): void {
+    const corporateTheme: CukThemeConfig = {
+      primary: '#1A73E8',
+      primaryDark: '#1765cf',
+      secondary: '#5f6368',
+      secondaryDark: '#4d5156',
+    };
+    this.themeService.applyTheme(corporateTheme);
+  }
 }
